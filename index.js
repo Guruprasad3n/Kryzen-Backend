b2680e64d985e22fd28b7132a63790f412bce5fc
Guruprasad3n <gp76652@gmail.com>
2024-06-08 16:59:25 +0530

    Initial Code

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
 const connectDB = require("./Config/db");
+const userRouter = require("./Routes/UserRouter");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+// app.get("/", async (req, res) => [res.send("Hello")]);
 
-app.get("/", async (req, res) => [res.send("Hello")]);
-
+app.use("/api/user", userRouter);
 const PORT = process.env.PORT || 8080;
 connectDB();
 app.listen(PORT, () => {
